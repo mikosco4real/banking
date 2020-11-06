ae3f04f959ef67cd081df01f4a6099d94df0abe4
Michael Okolo <okolomikel2009@gmail.com>
2020-11-06 13:18:50 +1100

    Added a basic banking application with methods for deposit, withdrawal, transfer and check balance

---
+++ banking.py
@@ -0,0 +1,25 @@
+class Account:
+
+    _balance = 0
+    id = None
+
+    def __init__(self, id):
+        self.id = id
+
+    def check_balance(self):
+        return self._balance
+
+    def deposit(self, amount):
+        self._balance += amount
+        return True
+
+    def withdraw(self, amount):
+        if self._balance > amount:
+            self._balance -= amount
+            return True
+        else:
+            return False
+
+    def transfer(self, account, amount):
+        account.withdraw(amount)
+        self.deposit(amount)
